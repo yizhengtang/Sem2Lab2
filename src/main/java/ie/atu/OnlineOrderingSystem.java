9794c20f62133ae3207de41727ebcbba51a5dbec
G00425075@atu.ie <g00425075@atu.ie>
2024-02-09 12:24:40 +0000

    In the main, I added arraylists and some advance for loops to add in different items and print them out.

--- src/main/java/ie/atu/OnlineOrderingSystem.java
+++ src/main/java/ie/atu/OnlineOrderingSystem.java
@@ -1,7 +1,26 @@
 package ie.atu;
 
+import java.util.ArrayList;
+
 public class OnlineOrderingSystem {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Online Ordering System.");
+                ArrayList <MenuItem> menuItems = new ArrayList<MenuItem>() ;
+                menuItems.add(new Burger ("Classic Burger", "Beef Patty, Lettuce, Tomato, Cheese", 8.99));
+                menuItems.add(new Pizza ("Margherita Pizza", "Tomato Sauce, Mozzarella, Basil", 12.99));
+                menuItems.add(new Salad ("Caesar Salad", "Romaine Lettuce, Croutons, Caesar Dressing", 6.99));
+
+        for (MenuItem menuItem : menuItems){
+            System.out.println("Name: " + menuItem.getName());
+            System.out.println("Description: " + menuItem.getDescription());
+            System.out.println("Price: $" + menuItem.getPrice() + "\n");
+        }
+
+        double totalCost = 0;
+        for (MenuItem menuItem : menuItems){
+            totalCost = totalCost + menuItem.getPrice();
+        }
+        System.out.println("Total Cost: $" + totalCost);
+
     }
 }
