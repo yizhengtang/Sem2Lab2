432b2e4d3a6ec573a3cc644ab76bd2ded8f60746
G00425075@atu.ie <g00425075@atu.ie>
2024-02-09 11:50:42 +0000

    Added 3 abstract methods in the MenuItem, and in the three different classes that implements MenuItem I added methods to provide concrete implementation for these abstract methods.

--- src/main/java/ie/atu/Pizza.java
+++ src/main/java/ie/atu/Pizza.java
@@ -1,4 +1,29 @@
 package ie.atu;
 
 public class Pizza implements MenuItem{
+    private String name;
+    private String ingredients;
+    private double price;
+
+    public Pizza(String name, String ingredients, double price){
+        this.name = name;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return ingredients;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
 }
--- src/main/java/ie/atu/Salad.java
+++ src/main/java/ie/atu/Salad.java
@@ -1,4 +1,27 @@
 package ie.atu;
 
 public class Salad implements MenuItem{
+    private String name;
+    private String ingredients;
+    private double price;
+
+    public Salad (String name, String ingredients, double price){
+        this.name = name;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return ingredients;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
 }
